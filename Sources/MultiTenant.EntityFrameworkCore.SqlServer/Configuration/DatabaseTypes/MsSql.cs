d65780c68cc10b8da25edc31bc35d5a4f1c2252d
Miguel Angulo Loria <mangulo@mdgdevelopers.com>
2020-06-24 22:51:48 -0600

    Xml Documentations

--- Sources/MultiTenant.EntityFrameworkCore.SqlServer/Configuration/DatabaseTypes/MsSql.cs
+++ Sources/MultiTenant.EntityFrameworkCore.SqlServer/Configuration/DatabaseTypes/MsSql.cs
@@ -4,13 +4,16 @@ using MultiTenant.EntityFrameworkCore.Configuration.DatabaseTypes;
 
 namespace MultiTenant.EntityFrameworkCore.SqlServer.Configuration.DatabaseTypes
 {
+    /// <inherit/>
     public class MsSql : IDatabaseType
     {
+        /// <inherit/>
         public IServiceCollection EnableDatabase(IServiceCollection services)
         {
             return services;
         }
 
+        /// <inherit/>
         public DbContextOptionsBuilder<TContext> SetConnectionString<TContext>(DbContextOptionsBuilder<TContext> contextOptionsBuilder, string connectionString) where TContext : DbContext
         {
             return contextOptionsBuilder.UseSqlServer(connectionString);
