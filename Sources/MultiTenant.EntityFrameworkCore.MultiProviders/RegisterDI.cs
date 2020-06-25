1e66053648ea44fc0b3203b830f6b6938b716ee6
Miguel Angulo Loria <mangulo@mdgdevelopers.com>
2020-06-24 22:47:14 -0600

    Add Multi Providers

---
+++ Sources/MultiTenant.EntityFrameworkCore.MultiProviders/RegisterDI.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MultiTenant.EntityFrameworkCore.Configuration.DatabaseTypes;
+using MultiTenant.EntityFrameworkCore.Data;
+using MultiTenant.EntityFrameworkCore.Data.Management;
+using System;
+using System.Linq;
+
+namespace MultiTenant.EntityFrameworkCore.MultiProviders
+{
+    /// <summary>
+    /// Register dependencies
+    /// </summary>
+    public static class RegisterDI
+    {
+        /// <summary>
+        /// Register dependencies to use MultiTenants
+        /// </summary>
+        /// <typeparam name="MultiTenantDbContext">DbContext which will be multi tenant</typeparam>
+        /// <typeparam name="TDataBaseManager">Implementation to obtain the connections strings corresponding to each tenant</typeparam>
+        /// <typeparam name="TDatabaseType">The class that implements the IDatabaseType interface, dependency injection will be done automatically</typeparam>
+        /// <param name="services">The services.</param>
+        /// <param name="tenantIdFieldName">HTTP header name to get the tenant ID</param>
+        /// <returns>The services.</returns>
+        public static IServiceCollection AddMultiTenantEntityFrameworkCore
+            <MultiTenantDbContext, 
+            TDataBaseManager,
+            TDatabaseType>(
+            this IServiceCollection services, 
+            string tenantIdFieldName)
+            where MultiTenantDbContext : DbContext, IDbContext
+            where TDataBaseManager : class, IDataBaseManager
+            where TDatabaseType : class, IDatabaseType
+        {
+            services.AddSingleton<IDatabaseType, TDatabaseType>();
+            var databaseTypeInstance = services.BuildServiceProvider().GetRequiredService<IDatabaseType>();
+
+            databaseTypeInstance.EnableDatabase(services);
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDbContext<MultiTenantDbContext>();
+
+            services.AddScoped<IDbContext, MultiTenantDbContext>();
+
+            services.AddTransient<IDataBaseManager, TDataBaseManager>();
+            services.AddTransient<IContextFactory>(x => new ContextFactory<MultiTenantDbContext>(tenantIdFieldName, x));
+
+            return services;
+        }
+    }
+}
